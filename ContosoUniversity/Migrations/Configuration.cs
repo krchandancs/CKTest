d4a0cd385bb4c21e635198d589638351aa256612
nikhil <nikhil.jain@kindlebit.com>
2019-12-17 16:05:56 +0530

    Configuration Updates

--- ContosoUniversity/Migrations/Configuration.cs
+++ ContosoUniversity/Migrations/Configuration.cs
@@ -17,6 +17,7 @@ namespace ContosoUniversity.Migrations
 
         protected override void Seed(SchoolContext context)
         {
+
             var students = new List<Student>
             {
                 new Student { FirstMidName = "Carson",   LastName = "Alexander", 
